ac118402858c3ef5f46bee7d3e379c5db940b750
Hadi <hadi@HaDis-MacBook-Pro.local>
2020-02-15 19:52:53 +0000

    modified scraper

--- Scraper.py
+++ Scraper.py
@@ -3,41 +3,46 @@ import os
 import urllib.request
 from bs4 import BeautifulSoup as Soup
 from selenium import webdriver
+import time
+
 
 
 driver = webdriver.Safari()
 URL = 'https://www.tesco.com/groceries/en-GB/search?query='
-file_path = 'items.txt'
+
+script_dir = os.path.dirname(__file__)
+file_path = "items.txt"
+abs_file_path = os.path.join(script_dir, file_path)
 
 
-def get_items(file_path, URL):
+def get_items(file, URL, script_dir):
     Full_URLS = []
-    ff = open("file_path", "r")
+    ff = open(file, "r")
     for f in ff:
         it, nits, pgs = f.split('-')
-        for i in range(pgs)
-            Full_URLS.append(URL+it+"&page="+i)
+        qq_path = "Tesco_"+it+".txt"
+        abs_qq_path = os.path.join(script_dir, qq_path)
+        qq = open(abs_qq_path, "w")
+        qq.write("***" + it + "***\n")
+        for i in range(int(pgs)):
+            driver.get(URL+it+"&page="+str(i))
+            page = driver.page_source
+            soup = Soup(page, 'lxml')
+            prices = soup.findAll('span', {'class': 'value'})
+            names = soup.findAll('h3', {'class': 'sc-chPdSV gRJrUF'})
+            items = []
+            for j in range(len(names) - 1):
+                qq.write(names[j].find(text=True) + " " + prices[j].find(text=True) + "\n")
+                items.append(names[j].find(text=True) + " " + prices[j].find(text=True))
+        qq.close()
+    ff.close()
     return Full_URLS
 
-def get_items_url(URL):
 
-    driver.get(URL)
-    # a = input("press enter")
-    page = driver.page_source
-
-    soup = Soup(page, 'lxml')
-    prices = soup.findAll('span', {'class':'value'})
-    names = soup.findAll('h3', {'class':'sc-chPdSV gRJrUF'})
-    items = []
-
-    for i in range(len(names)-1):
-        print(names[i].find(text=True) + " " + prices[i].find(text=True))
-        items.append(names[i].find(text=True) + " " + prices[i].find(text=True))
-
-
-Full_URLS = get_items(file_path, URL)
-get_items_url(Full_URLS)
+get_items(abs_file_path, URL, script_dir)
 driver.close()
+
+#get_items_url(Full_URLS)
 #download_images(images_url)
 #f.close()
 
