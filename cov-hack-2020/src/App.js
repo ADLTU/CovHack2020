75274df9c346614b5eb7210d7c70c8116895f6e3
abduljamac <abduljama001@gmail.com>
2020-02-15 23:18:28 +0000

    initialised react

--- cov-hack-2020/src/App.js
+++ cov-hack-2020/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  state = [ 'Chicken', 'Beef', 'Lamb', 'Pork', 'Cereals', 'Rice', 'Pasta', 'Noodles']
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       { this.state.map(function(item, i){
+          return <h1 key={i}> {item}</h1>
+        })
+       }
     </div>
   );
 }
